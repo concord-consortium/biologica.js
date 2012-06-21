f603e696f6025f1b6bc9eb8946bdb9ba1906f3d6
Sam Fentress <sfentress@concord.org>
2012-06-21 12:42:11 -0400

    Reformat traits test

--- test/spec/TraitsSpec.js
+++ test/spec/TraitsSpec.js
@@ -1,70 +1,73 @@
 describe("The characteristic", function() {
 
-  var phenotypeTests = [
-  //   Sex               Alleles       Trait       Characteristic //
-    [BioLogica.FEMALE,  "a:W",        "wings",      "Wings"],
-    [BioLogica.FEMALE,  "a:W,b:W",    "wings",      "Wings"],
-    [BioLogica.FEMALE,  "a:W,b:w",    "wings",      "Wings"],
-    [BioLogica.FEMALE,  "a:w,b:w",    "wings",      "No wings"],
+  var female = BioLogica.FEMALE,
+      male   = BioLogica.MALE,
 
-    [BioLogica.FEMALE,  "a:T",        "tail",       "Long tail"],
-    [BioLogica.FEMALE,  "a:T,b:Tk",   "tail",       "Long tail"],
-    [BioLogica.FEMALE,  "a:T,b:t",    "tail",       "Long tail"],
-    [BioLogica.FEMALE,  "a:Tk,b:Tk",  "tail",       "Kinked tail"],
-    [BioLogica.FEMALE,  "a:Tk,b:t",   "tail",       "Kinked tail"],
-    [BioLogica.FEMALE,  "a:t,b:t",    "tail",       "Short tail"],
+      phenotypeTests = [
+  //   Sex      Alleles          Trait         Characteristic   //
+    [ female,    "a:W",           "wings",        "Wings"         ],
+    [ female,    "a:W,b:W",       "wings",        "Wings"         ],
+    [ female,    "a:W,b:w",       "wings",        "Wings"         ],
+    [ female,    "a:w,b:w",       "wings",        "No wings"      ],
 
-    [BioLogica.FEMALE,  "a:H",        "horns",      "Hornless"],
-    [BioLogica.FEMALE,  "a:H,b:h",    "horns",      "Hornless"],
-    [BioLogica.FEMALE,  "a:h,b:h",    "horns",      "Horns"],
+    [ female,    "a:T",           "tail",         "Long tail"     ],
+    [ female,    "a:T,b:Tk",      "tail",         "Long tail"     ],
+    [ female,    "a:T,b:t",       "tail",         "Long tail"     ],
+    [ female,    "a:Tk,b:Tk",     "tail",         "Kinked tail"   ],
+    [ female,    "a:Tk,b:t",      "tail",         "Kinked tail"   ],
+    [ female,    "a:t,b:t",       "tail",         "Short tail"    ],
 
-    [BioLogica.FEMALE,  "a:Fl",       "forelimbs",  "Forelimbs"],
-    [BioLogica.FEMALE,  "a:fl,b:fl",  "forelimbs",  "No forelimbs"],
+    [ female,    "a:H",           "horns",        "Hornless"      ],
+    [ female,    "a:H,b:h",       "horns",        "Hornless"      ],
+    [ female,    "a:h,b:h",       "horns",        "Horns"         ],
 
-    [BioLogica.FEMALE,  "a:Hl",       "hindlimbs",  "Hindlimbs"],
-    [BioLogica.FEMALE,  "a:hl,b:hl",  "hindlimbs",  "No hindlimbs"],
+    [ female,    "a:Fl",          "forelimbs",    "Forelimbs"     ],
+    [ female,    "a:fl,b:fl",     "forelimbs",    "No forelimbs"  ],
 
-    [BioLogica.FEMALE,  "a:A1,b:A1",  "armor",      "Five armor"],
-    [BioLogica.FEMALE,  "a:A1,b:A2",  "armor",      "Five armor"],
-    [BioLogica.FEMALE,  "a:A1,b:a",   "armor",      "Three armor"],
-    [BioLogica.FEMALE,  "a:A2,b:A2",  "armor",      "Three armor"],
-    [BioLogica.FEMALE,  "a:A2,b:a",   "armor",      "One armor"],
-    [BioLogica.FEMALE,  "a:a,b:a",    "armor",      "No armor"],
+    [ female,    "a:Hl",          "hindlimbs",    "Hindlimbs"     ],
+    [ female,    "a:hl,b:hl",     "hindlimbs",    "No hindlimbs"  ],
 
-    [BioLogica.FEMALE,  "a:Rh",       "nose spike", "Nose spike"],
-    [BioLogica.FEMALE,  "a:rh,rh",    "nose spike", "No nose spike"],
-    [BioLogica.MALE,    "a:Rh",       "nose spike", "Nose spike"],
-    [BioLogica.MALE,    "a:rh",       "nose spike", "No nose spike"],
+    [ female,    "a:A1,b:A1",     "armor",        "Five armor"    ],
+    [ female,    "a:A1,b:A2",     "armor",        "Five armor"    ],
+    [ female,    "a:A1,b:a",      "armor",        "Three armor"   ],
+    [ female,    "a:A2,b:A2",     "armor",        "Three armor"   ],
+    [ female,    "a:A2,b:a",      "armor",        "One armor"     ],
+    [ female,    "a:a,b:a",       "armor",        "No armor"      ],
 
-    [BioLogica.FEMALE,  "a:c,b:c",                      "color",      "Frost"],
-    [BioLogica.FEMALE,  "a:C,a:M,b:M,a:B,a:D",          "color",      "Steel"],
-    [BioLogica.FEMALE,  "a:C,b:c,a:M,b:m,a:B,a:D,b:d",  "color",      "Steel"],
-    [BioLogica.FEMALE,  "a:C,a:M,b:M,a:b,b:b,a:D",      "color",      "Copper"],
-    [BioLogica.MALE,    "a:C,a:M,b:M,a:b,a:D",          "color",      "Copper"],
-    [BioLogica.FEMALE,  "a:C,a:M,b:M,a:B,a:d,b:d",      "color",      "Argent"],
-    [BioLogica.MALE,    "a:C,a:M,b:M,a:B,a:d",          "color",      "Argent"],
-    [BioLogica.FEMALE,  "a:C,a:M,b:m,a:b,b:b,a:d,b:d",  "color",      "Gold"],
-    [BioLogica.MALE,    "a:C,a:M,b:m,a:b,a:d",          "color",      "Gold"],
-    [BioLogica.FEMALE,  "a:C,a:m,b:m,a:B,a:D",          "color",      "Charcoal"],
-    [BioLogica.FEMALE,  "a:C,a:m,b:m,a:b,b:b,a:D",      "color",      "Earth"],
-    [BioLogica.MALE,    "a:C,a:m,b:m,a:b,a:D",          "color",      "Earth"],
-    [BioLogica.FEMALE,  "a:C,a:m,b:m,a:B,a:d,b:d",      "color",      "Dust"],
-    [BioLogica.MALE,    "a:C,a:m,b:m,a:B,a:d",          "color",      "Dust"],
-    [BioLogica.FEMALE,  "a:C,a:m,b:m,a:b,b:b,a:d,b:d",  "color",      "Sand"],
-    [BioLogica.MALE,    "a:C,a:m,b:m,a:b,a:d",          "color",      "Sand"],
+    [ female,    "a:Rh",          "nose spike",   "Nose spike"    ],
+    [ female,    "a:rh,rh",       "nose spike",   "No nose spike" ],
+    [ male,      "a:Rh",          "nose spike",   "Nose spike"    ],
+    [ male,      "a:rh",          "nose spike",   "No nose spike" ],
 
-    [BioLogica.FEMALE,  "a:M,b:M,a:dl,b:dl,a:A1,b:A1",  "liveliness", "Dead"],
-    [BioLogica.FEMALE,  "a:M,b:M,a:d,b:dl,a:A1",        "liveliness", "Dead"],
-    [BioLogica.FEMALE,  "a:M,b:m,a:D,b:dl,a:A2",        "liveliness", "Dead"],
-    [BioLogica.Male,    "a:dl",                         "liveliness", "Dead"],
-    [BioLogica.FEMALE,  "a:M,b:M,a:D,b:D,a:a,b:a",      "liveliness", "Alive"],
-    [BioLogica.Male,    "a:D",                          "liveliness", "Alive"],
-    [BioLogica.Male,    "a:d",                          "liveliness", "Alive"]
+    [ female,    "a:c,b:c",                      "color",      "Frost"    ],
+    [ female,    "a:C,a:M,b:M,a:B,a:D",          "color",      "Steel"    ],
+    [ female,    "a:C,b:c,a:M,b:m,a:B,a:D,b:d",  "color",      "Steel"    ],
+    [ female,    "a:C,a:M,b:M,a:b,b:b,a:D",      "color",      "Copper"   ],
+    [ male,      "a:C,a:M,b:M,a:b,a:D",          "color",      "Copper"   ],
+    [ female,    "a:C,a:M,b:M,a:B,a:d,b:d",      "color",      "Argent"   ],
+    [ male,      "a:C,a:M,b:M,a:B,a:d",          "color",      "Argent"   ],
+    [ female,    "a:C,a:M,b:m,a:b,b:b,a:d,b:d",  "color",      "Gold"     ],
+    [ male,      "a:C,a:M,b:m,a:b,a:d",          "color",      "Gold"     ],
+    [ female,    "a:C,a:m,b:m,a:B,a:D",          "color",      "Charcoal" ],
+    [ female,    "a:C,a:m,b:m,a:b,b:b,a:D",      "color",      "Earth"    ],
+    [ male,      "a:C,a:m,b:m,a:b,a:D",          "color",      "Earth"    ],
+    [ female,    "a:C,a:m,b:m,a:B,a:d,b:d",      "color",      "Dust"     ],
+    [ male,      "a:C,a:m,b:m,a:B,a:d",          "color",      "Dust"     ],
+    [ female,    "a:C,a:m,b:m,a:b,b:b,a:d,b:d",  "color",      "Sand"     ],
+    [ male,      "a:C,a:m,b:m,a:b,a:d",          "color",      "Sand"     ],
+
+    [ female,    "a:M,b:M,a:dl,b:dl,a:A1,b:A1",  "liveliness", "Dead"     ],
+    [ female,    "a:M,b:M,a:d,b:dl,a:A1",        "liveliness", "Dead"     ],
+    [ female,    "a:M,b:m,a:D,b:dl,a:A2",        "liveliness", "Dead"     ],
+    [ male,      "a:dl",                         "liveliness", "Dead"     ],
+    [ female,    "a:M,b:M,a:D,b:D,a:a,b:a",      "liveliness", "Alive"    ],
+    [ male,      "a:D",                          "liveliness", "Alive"    ],
+    [ male,      "a:d",                          "liveliness", "Alive"    ]
   ];
 
   for (var i=0,ii=phenotypeTests.length; i<ii; i++ ) {
     var test = phenotypeTests[i],
-        desc = "for "+test[2]+" of a " + (test[0] === BioLogica.FEMALE ? "female" : "male") +
+        desc = "for "+test[2]+" of a " + (test[0] === female ? "female" : "male") +
                " with "+test[1]+" should be: "+test[3].toLowerCase();
     it (desc, function() {
       expect([test[0], test[1]]).toHaveCharacteristic(test[2], test[3]);
